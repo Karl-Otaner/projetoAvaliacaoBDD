414abf67a583bf5547c6c3c447232655bbff2f08
Carlos Silva <carlos.silva>
2020-01-22 23:53:23 -0300

    Commit - 22/01/20 - 2º - Apanhando

---
+++ src/main/java/br/com/rsi/hub_bdd/steps/runners/Runners.java
@@ -0,0 +1,19 @@
+package br.com.rsi.hub_bdd.steps.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/feature",
+						glue = "br.com.br.rsi.hub_bdd.steps"
+
+		
+		
+		)
+
+
+public class Runners {
+
+}
---
+++ src/test/resources/Feature/Cadastro_Usuario.feature
@@ -0,0 +1,14 @@
+# language: pt
+Funcionalidade: Cadastro de novo usuario
+  
+  usuario devera fazer um cadastro
+  
+  Contexto:
+  
+  Dado: que eu estou no site de compras online
+  E nao estou logado
+  Quando clico no botao usuario
+  E clico para criar uma nova conta
+  
+
+  
